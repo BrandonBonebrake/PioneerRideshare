92ab890d15749162910081c514fc1f070f037faf
Brandon Bonebrake <bbonebrake67@gmail.com>
2019-11-12 20:00:46 -0600

    Trying to find the best way to crete the GUI
    Trying to find the best way to write the GUI. This is the current way it is being written but it will likely change.

--- src/gui/PioneerApplication.java
+++ src/gui/PioneerApplication.java
@@ -1,5 +1,63 @@
 package gui;
 
-public class PioneerApplication
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PioneerApplication extends Application
 {
+    // Global Variables
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        //Parent root = FXMLLoader.load(getClass().getResource("pioneerApplication.fxml"));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("pioneerApplication.fxml")));
+
+        // Change the properties of the scene here
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        // Change the properties of the stage here
+        primaryStage.setTitle("Pioneer Rideshare");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    public void buttonExitClicked() {
+        System.exit(0);
+    }
+
+    @FXML
+    public void buttonOfferClicked(){
+        System.out.println("Ride Offer");
+    }
+
+    @FXML
+    public void buttonRequestClicked(){
+        System.out.println("Ride Request");
+    }
+
+    @FXML
+    public void buttonViewRidesClicked() {
+        System.out.println("View Rides");
+    }
+
+    @FXML
+    public void buttonLoginSignupClicked() {
+        System.out.println("Login/Signup");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/gui/SplashScreenPanel.java
+++ src/gui/SplashScreenPanel.java
@@ -1,5 +1,54 @@
 package gui;
 
-public class SplashScreenPanel
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class SplashScreenPanel extends StackPane
 {
+    Button exitBtn = null;
+
+    public SplashScreenPanel() throws IOException
+    {
+       super();
+
+       this.createComponents();
+    }
+
+    private void createComponents() throws IOException
+    {
+        this.getStylesheets().add("gui/splashScreenPanel.fxml");
+
+        this.createExitButton();
+    }
+
+    private void createExitButton()
+    {
+        // All of the styling for the button is done here
+        String style = "-fx-background-color: linear-gradient(#ffd65b, #e68400)," +
+                       "linear-gradient(#ffef84, #f2ba44)," +
+                       "linear-gradient(#ffea6a, #efaa22);" +
+                       "fx-background-radius: 50;" +
+                       "-fx-font-weight: bold;" +
+                       "-fx-font-size: 18px;" +
+                       "-fx-text-fill: blue";
+
+        exitBtn = new Button("Exit");
+        exitBtn.setPrefSize(100, 50);
+        exitBtn.setOnAction(e -> this.buttonRideOfferClicked());
+        //exitBtn.setStyle(style);
+        exitBtn.getStyleClass().removeAll();
+        exitBtn.getStyleClass().add("/gui/style.css");
+
+        this.getChildren().add(exitBtn);
+    }
+
+
+    public void buttonRideOfferClicked()
+    {
+        System.exit(0);
+    }
 }
