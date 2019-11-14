2a674fa3224c6ee8effafcc06eff5f4fa94684a7
Brandon Bonebrake <bbonebrake67@gmail.com>
2019-11-13 21:25:17 -0600

    Update gitIgnore and added back button
    Updated the gitIgnore to skip the production files. Should make that better. Added a way to go back without closing the program.

--- src/gui/RideListingPanel.java
+++ src/gui/RideListingPanel.java
@@ -1,13 +1,19 @@
 package gui;
 
+import date.Date;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import location.Location;
 import ride.Ride;
+import student.Student;
 
 public class RideListingPanel
 {
@@ -19,6 +25,7 @@ public class RideListingPanel
     private Stage stage = null;
     private Pane pane = null;
     private TableView table = null;
+    private Button backBtn = null;
 
     private final ObservableList<String> data =
             FXCollections.observableArrayList(
@@ -37,20 +44,45 @@ public class RideListingPanel
 
     private void createComponents()
     {
+        this.createTable();
+        this.createBackButton();
+    }
+
+    private void createBackButton()
+    {
+        backBtn = new Button("Back");
+        backBtn.setPrefSize(100, 50);
+        backBtn.setOnAction(e -> this.buttonBackClicked());
+
+        pane.getChildren().add(backBtn);
+    }
+
+    private void buttonBackClicked()
+    {
+        SplashScreenPanel splash = new SplashScreenPanel(stage);
+
+        stage.setScene(new Scene(splash.getPane(), 1280, 720));
+    }
+
+    private void createTable()
+    {
         final double CELL_WIDTH = WIDTH / 8.0 + 5;
         table = new TableView();
         table.setEditable(false);
 
-        TableColumn offerRequest = new TableColumn("Offer/Request");
-        TableColumn leaveCityState = new TableColumn("Leaving");
-        TableColumn destinationCityState = new TableColumn("Destination");
-        TableColumn location = new TableColumn("City/Sate");
-        TableColumn leaveDateTime = new TableColumn("Leaving");
-        TableColumn returnDateTime = new TableColumn("Returning");
-        TableColumn dateTime = new TableColumn("Date/Time");
-        TableColumn email = new TableColumn("Email @ uwplatt.edu");
-        TableColumn request = new TableColumn("Request to Join/Offer to Drive");
 
+        // Create all the columns that will represent the different data points we will display to the user
+        TableColumn<String, Ride> offerRequest = new TableColumn("Offer/Request");
+        TableColumn<String, Location> leaveCityState = new TableColumn("Leaving");
+        TableColumn<String, Location> destinationCityState = new TableColumn("Destination");
+        TableColumn location = new TableColumn("City/Sate");
+        TableColumn<String, Date> leaveDateTime = new TableColumn("Leaving");
+        TableColumn<String, Date> returnDateTime = new TableColumn("Returning");
+        TableColumn dateTime = new TableColumn("Date/Time");
+        TableColumn<String, Student> email = new TableColumn("Email @ uwplatt.edu");
+        TableColumn<String, Button> request = new TableColumn("Request to Join/Offer to Drive");
+
+        // Set the column widths
         offerRequest.setPrefWidth(105);
         leaveCityState.setPrefWidth(CELL_WIDTH);
         destinationCityState.setPrefWidth(CELL_WIDTH);
@@ -59,6 +91,7 @@ public class RideListingPanel
         email.setPrefWidth(WIDTH / 5.0);
         request.setPrefWidth(WIDTH / 5.0);
 
+        // Add the sub-columns to the columns
         location.getColumns().addAll(leaveCityState, destinationCityState);
         dateTime.getColumns().addAll(leaveDateTime, returnDateTime);
 
@@ -70,12 +103,9 @@ public class RideListingPanel
         //email.setCellFactory(new PropertyValueFactory<>("Email @uwplatt.edu"));
         //request.setCellFactory(new PropertyValueFactory<>("Request to Join/Offer to Drive"));
 
-        //Ride ride = new Ride();
-
-        //table.getItems().add(ride);
         table.getColumns().addAll(offerRequest, location, dateTime, email, request);
         table.setPrefSize(WIDTH, HEIGHT - 20);
-        table.setTranslateY(30);
+        table.setTranslateY(50);
 
         pane.getChildren().add(table);
     }
