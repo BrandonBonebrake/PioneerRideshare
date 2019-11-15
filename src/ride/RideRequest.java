12ecd8dbee474d47c199b9e4a1be6261e6913237
biermanb <biermanb@users.noreply.github.com>
2019-11-14 20:13:28 -0600

    Ride, RideOffer, RideRequest Updates
    The Ride class has been mostly completed and commented. The RideOffer and RideRequest classes were for the most part completed for this point in time and were all fully commented with details. Some updates may be needed in the future to integrate with the rest of the program

--- src/ride/RideRequest.java
+++ src/ride/RideRequest.java
@@ -1,5 +1,35 @@
 package ride;
 
-public class RideRequest
+import date.PioneerDate;
+import location.Location;
+import student.Student;
+import time.PioneerTime;
+
+/**
+ This class is utilized to create RideRequest objects which
+ hold all pertinent ride information for the program.
+ The data member isOffer is set to false.
+
+ @author Bryce Bierman
+ **/
+
+public class RideRequest extends Ride
 {
+    /**
+     Constructor for the RideRequest class. Holds all pertinent ride information.
+     The data member isOffer is set to false.
+
+     @param departLoc   Depart Location for Ride
+     @param destination Destination for Ride
+     @param leaveDate   Leave date for Ride
+     @param returnDate  Return date for Ride
+     @param leaveTime   Leave time for Ride
+     @param returnTime  Return time for Ride
+     @param student     Student that created Ride
+     **/
+    public RideRequest(Location departLoc, Location destination, PioneerDate leaveDate, PioneerDate returnDate,
+                       PioneerTime leaveTime, PioneerTime returnTime, Student student)
+    {
+        super(departLoc, destination, leaveDate, returnDate, leaveTime, returnTime, false, student);
+    }
 }
