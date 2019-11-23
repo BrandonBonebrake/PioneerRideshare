b7fc973d5bdc52513395d96b42f5599d63852002
Brandon Bonebrake <bbonebrake67@gmail.com>
2019-11-23 16:07:47 -0600

    Updated GUI class descriptions
    Updated GUI class descriptions to reflect their function.

--- src/gui/LoginPanel.java
+++ src/gui/LoginPanel.java
@@ -2,13 +2,11 @@ package gui;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 final class LoginPanel extends DefaultView
 {
     // Global Variables
-    private Button backBtn = null;
 
     public LoginPanel(Stage stage, Scene splash, int width, int height)
     {
@@ -24,7 +22,7 @@ final class LoginPanel extends DefaultView
 
     private void createBackButton()
     {
-        backBtn =super.createButton("Back", 200, 75,
+        Button backBtn =super.createButton("Back", 200, 75,
                 0, 0, PioneerApplication.EXIT_STYLE);
         backBtn.setOnAction(event -> buttonBackClicked());
     }
