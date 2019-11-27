6bed1e43d5db786719c7bb453746e740b902092d
biermanb <biermanb@users.noreply.github.com>
2019-11-26 21:03:45 -0600

    Login/Signup Panel Classes
    Wrote the Login/Signup Panel classes for the program, they are able to be accessed using the Login/Signup screen from the Splash Screen. More functionality to be implemented soon.

--- src/gui/LoginPanel.java
+++ src/gui/LoginPanel.java
@@ -2,11 +2,41 @@ package gui;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 final class LoginPanel extends DefaultView
 {
     // Global Variables
+
+    private TextField emailTextbox;
+    private TextField passwordTextbox;
+
+    private final int DEFAULT_X_COMP = 300;  // Sets components distance from x-axis
+    private final int DEFAULT_Y_COMP = 300; // Sets components distance from y-axis
+
+    private final int LOGIN_LABEL_X = 270;
+    private final int LOGIN_LABEL_Y = 125;
+
+    private final int DEFAULT_COMP_HEIGHT = 40;
+    private final int DEFAULT_COMP_WIDTH = 220;
+
+    private final int SIGNUP_BUTTON_WIDTH = 180;
+    private final int SIGNUP_X_TRANS = DEFAULT_X_COMP + 19;
+
+    private final int FORGOT_BUTTON_WIDTH = 160;
+    private final int FORGOT_X_TRANS = DEFAULT_X_COMP + 28;
+
+    private final int DIST_Y_BETWEEN_COMPONENTS = 60; // Default y-dist between components
+
+    private final int EMAIL_LABEL_X = DEFAULT_X_COMP - 150;
+
+    final static String SIGNUP_STYLE = "-fx-background-color: linear-gradient(blue, black)," +
+            "linear-gradient(blue, darkBlue), linear-gradient(orange, #ffc266);" +
+            "-fx-background-radius: 0; -fx-font-weight: bold; -fx-font-size: 12px;" +
+            "-fx-text-fill: blue; -fx-border-color: orange; -fx-border-width: 0;";
 
     public LoginPanel(Stage stage, Scene splash, int width, int height)
     {
@@ -18,17 +48,104 @@ final class LoginPanel extends DefaultView
     void createComponents()
     {
         this.createBackButton();
+        this.createEmailTextbox();
+        this.createPasswordTextbox();
+        this.createLoginButton();
+        this.createSignupButton();
+        this.createForgotButton();
+        this.createLoginLabel();
+        this.createEmailLabel();
+        this.createPasswordLabel();
     }
 
     private void createBackButton()
     {
-        Button backBtn =super.createButton("Back", 200, 75,
+        Button backBtn = super.createButton("Back", 200, 75,
                 0, 0, PioneerApplication.EXIT_STYLE);
         backBtn.setOnAction(event -> buttonBackClicked());
+    }
+
+    private void createEmailTextbox()
+    {
+        emailTextbox = super.createTextField("example@uwplatt.edu", DEFAULT_COMP_WIDTH, DEFAULT_COMP_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP);
+    }
+
+    private void createPasswordTextbox()
+    {
+        passwordTextbox = super.createTextField("Password", DEFAULT_COMP_WIDTH, DEFAULT_COMP_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP + DIST_Y_BETWEEN_COMPONENTS);
+    }
+
+    private void createLoginButton()
+    {
+        Button loginButton = super.createButton("Login", DEFAULT_COMP_WIDTH, DEFAULT_COMP_HEIGHT, DEFAULT_X_COMP,
+                DEFAULT_Y_COMP + (2 * DIST_Y_BETWEEN_COMPONENTS), SIGNUP_STYLE);
+
+        loginButton.setOnAction(event -> buttonLoginClicked());
+    }
+
+    private void createSignupButton()
+    {
+        Button signupButton = super.createButton("New? Signup here!", SIGNUP_BUTTON_WIDTH, DEFAULT_COMP_HEIGHT,
+                SIGNUP_X_TRANS, DEFAULT_Y_COMP + (3 * DIST_Y_BETWEEN_COMPONENTS), SIGNUP_STYLE);
+
+        signupButton.setOnAction(event -> signupButtonClicked());
+    }
+
+    private void createForgotButton()
+    {
+        Button forgotPasswordButton = super.createButton("Forgot Password!", FORGOT_BUTTON_WIDTH, DEFAULT_COMP_HEIGHT,
+                FORGOT_X_TRANS, DEFAULT_Y_COMP + (4 * DIST_Y_BETWEEN_COMPONENTS), SIGNUP_STYLE);
+
+        forgotPasswordButton.setOnAction(event -> forgotButtonClicked());
+    }
+
+    private void createLoginLabel()
+    {
+        Label loginLabel = super.createLabel("Login", LOGIN_LABEL_X,
+                LOGIN_LABEL_Y);
+        loginLabel.setFont(Font.font(100));
+    }
+
+    private void createEmailLabel()
+    {
+        Label emailLabel = super.createLabel("   Email:", EMAIL_LABEL_X, DEFAULT_Y_COMP);
+        emailLabel.setFont(Font.font(24));
+    }
+
+    private void createPasswordLabel()
+    {
+        Label passwordLabel = super.createLabel("Password:", EMAIL_LABEL_X,
+                DEFAULT_Y_COMP +  DIST_Y_BETWEEN_COMPONENTS);
+        passwordLabel.setFont(Font.font(24));
     }
 
     private void buttonBackClicked()
     {
         super.returnView();
     }
+
+    private void buttonLoginClicked()
+    {
+        PopUpPanel.display("Login Successful");
+        super.returnView();
+    }
+
+    private void signupButtonClicked()
+    {
+        SignupPanel signupPanel = new SignupPanel(super.getStage(), super.getStage().getScene(),
+                super.getWidth(), super.getHeight());
+
+        super.changeScene(signupPanel.getPane());
+    }
+
+    private void forgotButtonClicked()
+    {
+        ForgotPasswordPanel forgotPasswordPanel = new ForgotPasswordPanel(super.getStage(), super.getStage().getScene(),
+                super.getWidth(), super.getHeight());
+
+        super.changeScene(forgotPasswordPanel.getPane());
+    }
+
 }
--- src/gui/SignupPanel.java
+++ src/gui/SignupPanel.java
@@ -1,10 +1,32 @@
 package gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 final class SignupPanel extends DefaultView
 {
+    private TextField emailTextbox;
+    private TextField passwordTextbox;
+    private TextField passwordTextbox2;
+
+    private final int SIGNUP_LABEL_X = 250;
+    private final int SIGNUP_LABEL_Y = 125;
+
+    private final int DEFAULT_X_COMP = 300;  // Sets components distance from x-axis
+    private final int DEFAULT_Y_COMP = 300; // Sets components distance from y-axis
+
+    private final int INPUT_TEXTBOX_HEIGHT = 40;
+    private final int INPUT_TEXTBOX_WIDTH = 220;
+
+    private final int DIST_Y_BETWEEN_COMPONENTS = 60;
+
+    private final int EMAIL_LABEL_X = DEFAULT_X_COMP - 150;
+    private final int REENTER_PASSWORD_LABEL_X = EMAIL_LABEL_X - 70;
+
     /**
      *
      *
@@ -16,11 +38,84 @@ final class SignupPanel extends DefaultView
     SignupPanel(Stage stage, Scene scene, int width, int height)
     {
         super(stage, scene, width, height);
+        this.createComponents();
     }
 
     @Override
     void createComponents()
     {
+        this.createBackButton();
+        this.createSignupLabel();
+        this.createEmailTextbox();
+        this.createPasswordTextbox();
+        this.createPasswordTextbox2();
+        this.createSubmitButton();
+        this.createEmailLabel();
+        this.createPasswordLabel();
+        this.createPasswordLabel2();
+    }
 
+    private void createBackButton()
+    {
+        Button backBtn = super.createButton("Back", 200, 75,
+                0, 0, PioneerApplication.EXIT_STYLE);
+        backBtn.setOnAction(event -> buttonBackClicked());
+    }
+
+    private void createSignupLabel()
+    {
+        Label signupLabel = super.createLabel("Signup", SIGNUP_LABEL_X,
+                SIGNUP_LABEL_Y);
+        signupLabel.setFont(Font.font(100));
+    }
+
+    private void createEmailTextbox()
+    {
+        emailTextbox = super.createTextField("example@uwplatt.edu", INPUT_TEXTBOX_WIDTH, INPUT_TEXTBOX_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP);
+
+    }
+
+    private void createPasswordTextbox()
+    {
+        passwordTextbox = super.createTextField("Password", INPUT_TEXTBOX_WIDTH, INPUT_TEXTBOX_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP + DIST_Y_BETWEEN_COMPONENTS);
+    }
+
+    private void createPasswordTextbox2()
+    {
+        passwordTextbox = super.createTextField("Password", INPUT_TEXTBOX_WIDTH, INPUT_TEXTBOX_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP + (2 * DIST_Y_BETWEEN_COMPONENTS));
+    }
+
+    private void createSubmitButton()
+    {
+        Button submitButton = super.createButton("Submit", INPUT_TEXTBOX_WIDTH, INPUT_TEXTBOX_HEIGHT,
+                DEFAULT_X_COMP, DEFAULT_Y_COMP + (3 * DIST_Y_BETWEEN_COMPONENTS), PioneerApplication.EXIT_STYLE);
+    }
+
+    private void createEmailLabel()
+    {
+        Label emailLabel = super.createLabel("   Email:", EMAIL_LABEL_X, DEFAULT_Y_COMP);
+        emailLabel.setFont(Font.font(24));
+    }
+
+    private void createPasswordLabel()
+    {
+        Label passwordLabel = super.createLabel("Password:", EMAIL_LABEL_X,
+                DEFAULT_Y_COMP +  DIST_Y_BETWEEN_COMPONENTS);
+        passwordLabel.setFont(Font.font(24));
+    }
+
+    private void createPasswordLabel2()
+    {
+        Label passwordLabel2 = super.createLabel("Reenter Password:", REENTER_PASSWORD_LABEL_X,
+                DEFAULT_Y_COMP + (2 * DIST_Y_BETWEEN_COMPONENTS));
+        passwordLabel2.setFont(Font.font(24));
+    }
+
+    private void buttonBackClicked()
+    {
+        super.returnView();
     }
 }
