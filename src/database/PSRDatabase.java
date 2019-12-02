b6026cd94ceb44d418db24b3ca6804a481ec922e
gm-lloyd <55302386+gm-lloyd@users.noreply.github.com>
2019-12-02 10:37:43 -0600

    added remove and getCurrentRides functions, still trying to figure out how to get it working for every machine

--- src/database/PSRDatabase.java
+++ src/database/PSRDatabase.java
@@ -7,6 +7,7 @@ import student.Student;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.regex.PatternSyntaxException;
 import java.lang.Class;
@@ -57,6 +58,13 @@ public class PSRDatabase
         return conn;
     }
 
+    /**
+     * Insert an item with the properties of "values" into "tableName"
+     * @param con
+     * @param tableName
+     * @param values
+     * @throws SQLException
+     */
     public static void tableInsert(Connection con, String tableName, String values) throws SQLException {
         Statement stmt = null;
         String query = "INSERT INTO " + "prsrideshare." + tableName + "(" + values + ")";
@@ -77,6 +85,77 @@ public class PSRDatabase
         }
     }
 
+    /**
+     * Remove an element from the table. "condition" needs to be a data value
+     * in the specific table, eg. F_NAME = Larry
+     * @param con
+     * @param tableName
+     * @param condition
+     * @throws SQLException
+     */
+    public static void tableRemove(Connection con, String tableName, String condition) throws SQLException {
+        Statement stmt = null;
+        String query = "DELETE FROM " + "prsrideshare." + tableName + " WHERE " + condition;
+        try
+        {
+            stmt = con.createStatement();
+            int rowsAffected = stmt.executeUpdate(query);
+            System.out.println("Query Ok \n" + rowsAffected + " Rows Affected");
+        } catch (SQLException e) {
+            System.out.println("Query Unsuccessful");
+            e.printStackTrace();
+        } finally
+        {
+            if (stmt != null) {stmt.close();};
+        }
+    }
+
+    public static List<String> getCurrentRides(Connection con)
+    {
+        List<String> list = null;
+        Statement stmt = null;
+        try
+        {
+            stmt = con.createStatement();
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            ResultSet myRS = null;
+            //myRS = stmt.executeQuery(
+                    //"SELECT * FROM " + "prsrideshare.current_rides");
+
+            while (myRS.next())
+            {
+                   list.add(myRS.getString("D_LOC") + ", " + myRS.getString("R_LOC") + ", " +
+                           myRS.getString("D_DATE") + ", " + myRS.getString("R_DATE") + ", " +
+                           myRS.getString("D_TIME") + ", " + myRS.getString("R_TIME") + ", " +
+                           myRS.getBoolean("IS_OFFER") + ", " + myRS.getString("STUDENT_EMAIL"));
+            }
+        } catch (SQLException e)
+        {
+            System.out.println("Query Unsuccessful");
+            e.printStackTrace();
+        }
+        return list;
+    }
+    /*
+    D_LOC	      VARCHAR(30),
+	R_LOC	      VARCHAR(30),
+	D_DATE        CHAR(10),
+	R_DATE        CHAR(10),
+	D_TIME        CHAR(5),
+	R_TIME        CHAR(5),
+	IS_OFFER      BOOLEAN,
+	STUDENT_EMAIL VARCHAR(30) PRIMARY KEY
+
+     */
+
+
+    /**
+     * Print out the tables in a specific database
+     * @param con
+     * @param dbName
+     * @throws SQLException
+     */
     public static void viewTable(Connection con, String dbName)
             throws SQLException {
 
