d3e7516145f121749aeaaf8b550067e59ac2d59c
Brandon Bonebrake <bbonebrake67@gmail.com>
2019-12-10 21:42:27 -0600

    Removed unused libraries
    Removed libraries that were being used while implemeting the email handler

--- src/database/EmailHandler.java
+++ src/database/EmailHandler.java
@@ -8,7 +8,7 @@ public class EmailHandler
     public static void send(String to, String subject, String content)
     {
         Email mail = new SimpleEmail();
-        String from = "pioneerridesharenoreply@gmail.com";
+        String from = "PioneerRideshareNoreply@gmail.com";
         String appPassword = "qniufxguzrcisnvx";
         try {
             mail.setFrom(from);
@@ -21,8 +21,6 @@ public class EmailHandler
             mail.setAuthentication(from, appPassword);
             mail.send();
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        catch (Exception ignored) {}
     }
 }
